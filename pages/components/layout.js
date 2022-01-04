5e7072bbea45c8150e69f5282e00eb3cdac3426b
withvictor <withvictor@gmail.com>
2022-01-05 00:40:17 +0800

    ga

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -2,24 +2,37 @@ import Header from "./layout/header";
 import Footer from "./layout/footer";
 import Ig from "./layout/ig";
 import Head from "next/head";
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-YWRDWGKB4J');
-const Layout = (props) => {
-  const { children, title, loading ,descript ,img } = props;
-  
+import Script from "next/script";
+const Layout = (props) => {  
+  const { children, title, loading, descript, img } = props;
   return (
     <>
       <Head>
-        <title>{title ? title + " ": ""}LyraBuy 美國代購</title>
+        <title>{title ? title + " " : ""}LyraBuy 美國代購</title>
         <meta charSet="UTF-8" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />        
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="description" content={descript} />
         <meta property="og:description" content={descript}></meta>
-        <meta property="og:title" content={title ? title + " LyraBuy 美國代購": "LyraBuy 美國代購"}></meta>
+        <meta
+          property="og:title"
+          content={title ? title + " LyraBuy 美國代購" : "LyraBuy 美國代購"}
+        ></meta>
         <meta property="og:type" content="LyraBuy 美國代購"></meta>
-        <meta property="og:image" content={img}/>
+        <meta property="og:image" content={img} />
         <meta name="keywords" content="Lyra,Buy,美國代購,LyraBuy" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />                
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-71M8ZRTSJR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-71M8ZRTSJR');
+        `}
+        </Script>
       </Head>
 
       <>
