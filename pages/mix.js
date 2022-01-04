8532e11aaee2476a66916dfc816a763f43ed18fe
withvictor <withvictor@gmail.com>
2022-01-04 23:54:22 +0800

    add mix

--- pages/mix.js
+++ pages/mix.js
@@ -1,41 +1,41 @@
-import { useState, useEffect } from "react";
-import { Button, Modal } from "react-bootstrap";
-import { PrismaClient } from "@prisma/client";
+// import { useState, useEffect } from "react";
+//import { Button, Modal } from "react-bootstrap";
+//import { PrismaClient } from "@prisma/client";
 
 // const prisma = new PrismaClient()
 
-const Mix = ({ dbdata }) => {
-  let showdata = "",hhh = "";
-  showdata = JSON.parse(dbdata);
-  const [fin, setFin] = useState(false);  
-    hhh = showdata.map((item,key) => {
+// const Mix = ({ dbdata }) => {
+//   let showdata = "",hhh = "";
+//   showdata = JSON.parse(dbdata);
+//   const [fin, setFin] = useState(false);  
+//     hhh = showdata.map((item,key) => {
       
-      return (
-        <div key={key}>
-          <h1>{item.title}</h1>
-        </div>
-      );
-    });
-  useEffect(() => {        
-    setFin(true);
-  }, []);
-  return (
-    <>
+//       return (
+//         <div key={key}>
+//           <h1>{item.title}</h1>
+//         </div>
+//       );
+//     });
+//   useEffect(() => {        
+//     setFin(true);
+//   }, []);
+//   return (
+//     <>
       
-       [{fin === true ? hhh : ''}] 
+//        [{fin === true ? hhh : ''}] 
        
       
-    </>
-  );
-};
+//     </>
+//   );
+// };
 
-export async function getStaticProps() {
-  const prisma = new PrismaClient();
-  const menu = await prisma.admin_menu.findMany();
-  let dbdata = JSON.stringify(menu);
+// export async function getStaticProps() {
+//   const prisma = new PrismaClient();
+//   const menu = await prisma.admin_menu.findMany();
+//   let dbdata = JSON.stringify(menu);
 
-  return {
-    props: { dbdata },
-  };
-}
-export default Mix;
+//   return {
+//     props: { dbdata },
+//   };
+// }
+// export default Mix;
