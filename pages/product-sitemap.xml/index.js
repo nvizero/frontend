13cb14bf47f381b4ac763668abcc68a61ff6541d
withvictor <withvictor@gmail.com>
2022-01-07 22:15:08 +0800

    fix sitemap

--- pages/product-sitemap.xml/index.js
+++ pages/product-sitemap.xml/index.js
@@ -2,13 +2,13 @@
 import { getServerSideSitemap } from "next-sitemap";
 
 export const getServerSideProps  = async (cts) => { 
-    let url = `${process.env.PRODUCT_API}${"api/v1/productList"}`;
+    let url = `http://lyra-buy.com/${"api/v1/productList"}`;
     console.log(url);
     const response = await fetch(url);
     const list  = await response.json();
 
     const fields = list.map((row) =>({
-        loc: `${process.env.PRODUCT_API}/product/${row.id}`,
+        loc: `http://lyra-buy.com/product/${row.id}`,
         lastmod: new Date().toISOString(),
     }));
 
