e9f4f2884478f1c3c0dd9793d4dd6ef78ee3d32c
withvictor <withvictor@gmail.com>
2022-01-18 22:12:15 +0800

    no sync config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  env: {    
-    PRODUCT_API: 'http://127.0.0.1:2699/',
-    image_url: "http://127.0.0.1:2699/"
-  },
+module.exports = {  
+  siteUrl:'http://localhost:3000/',
+  env: {
+    PRODUCT_API: "http://127.0.0.1:2699/",
+    image_url: "http://127.0.0.1:2699/",
+  },  
 };
