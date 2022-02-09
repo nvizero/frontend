57f97cd4576cdadfd089cf66698b4af497c02ff8
withvictor <withvictor@gmail.com>
2022-02-09 19:57:01 +0800

    site map

--- pages/product-sitemap.xml/index.js
+++ pages/product-sitemap.xml/index.js
@@ -2,7 +2,7 @@
 import { getServerSideSitemap } from "next-sitemap";
 
 export const getServerSideProps  = async (cts) => { 
-    let url = `http://lyra-buy.com/${"api/v1/allProducts"}`;    
+    let url = `${process.env.PRODUCT_API}${"api/v1/allProducts"}`;    
     const response = await fetch(url);
     const list  = await response.json();
     const fields = list.map((row) =>({
